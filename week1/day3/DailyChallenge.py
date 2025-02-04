345f087f6603e6d17bdc43cf66961d131ad91eec
florencia-ogorinsky <florencia.ogorinsky@gmail.com>
2025-02-04 21:26:58 +0200

    exercise

--- week1/day3/DailyChallenge.py
+++ week1/day3/DailyChallenge.py
@@ -2,26 +2,43 @@
 # Ask a user for a word
 # Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
 
-# word=input("Write a word ")
-# dicta=dict()
-# lista=list(dicta)
-# listb=list()
-
-# a=0
-# b=-1
-# for x in word:
-#     lista.append(x)
-#     b+=1
-#     listb.append(b)
-#     print(lista[a],listb[a])
-#     a+=1
-# print(lista)
-# print(listb)
+word=input("Write a word ")
+dicta=dict()
+lista=list(dicta)
+listb=list()
 
 
-# for x in lista:
-#     g=lista.index(x)
-#     print(x,g)
+
+a=0
+b=0
+for x in word:
+    lista.append(x)
+    listb.append(b)
+    b+=1
+print(lista)
+print(listb)
+
+
+new_dictt=dict(zip(listb,lista))
+
+
+print(new_dictt)
+
+
+list_values=list(new_dictt.values())
+list_keys=list(new_dictt.keys())
+
+print(list_values)
+print(list_keys)
+
+
+
+listNew=list(zip(list_values,list_keys))
+print(listNew)
+
+
+
+
 
 
 # Make sure the letters are the keys.
@@ -39,46 +56,46 @@
 # Examples
 
 
-things={"bottle":"$8",
- "mousse":"$20",
- "screen":"$89",
- "dog":"$500",
- "bed":"$260",
- "apartment":"$546455"
-}
+# things={"bottle":"$8",
+#  "mousse":"$20",
+#  "screen":"$89",
+#  "dog":"$500",
+#  "bed":"$260",
+#  "apartment":"$546455"
+# }
 
-print(things["bottle"].removeprefix("$"))
-listf=list()
+# print(things["bottle"].removeprefix("$"))
+# listf=list()
 
-for x in things:
-    listf.append(things[x].removeprefix("$"))
-listg=list(things.keys())
-things2=dict(zip(listg,listf))
-print(things2)
-
-
-
-list_with_int=[]
-for v in things2.values():
-    list_with_int.append((int(v)))
-print(list_with_int)
+# for x in things:
+#     listf.append(things[x].removeprefix("$"))
+# listg=list(things.keys())
+# things2=dict(zip(listg,listf))
+# print(things2)
 
 
-new_dict=dict(zip(things2.keys(),list_with_int))
-print(new_dict)
 
-import random
+# list_with_int=[]
+# for v in things2.values():
+#     list_with_int.append((int(v)))
+# print(list_with_int)
 
-budget=int(input("Write in numbers your budget with no dollar sign, just number "))
 
-affordable_items = {}
+# new_dict=dict(zip(things2.keys(),list_with_int))
+# print(new_dict)
 
-for k, v in new_dict.items():
-    if v < budget:
-        affordable_items[k] = v
+# import random
 
-keys_within_budget = list(affordable_items.keys())
-random.shuffle(keys_within_budget)
+# budget=int(input("Write in numbers your budget with no dollar sign, just number "))
 
-print(keys_within_budget)
+# affordable_items = {}
+
+# for k, v in new_dict.items():
+#     if v < budget:
+#         affordable_items[k] = v
+
+# keys_within_budget = list(affordable_items.keys())
+# random.shuffle(keys_within_budget)
+
+# print(keys_within_budget)
 

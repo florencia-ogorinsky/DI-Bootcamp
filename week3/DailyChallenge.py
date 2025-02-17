ddf329316cdc620be4a79ffaa8c38b422103dc8f
florencia-ogorinsky <florencia.ogorinsky@gmail.com>
2025-02-17 14:29:10 +0200

    d

---
+++ week3/DailyChallenge.py
@@ -0,0 +1,15 @@
+# Step 1: I am going to talk about COVID-19 because data really helped governments and organizations make decisions to control the pandemic. 
+
+# Step 2: 
+
+# Tracking the Spread: They analyzed daily case counts to predict where the virus would spread next, and whether new restrictions were needed.
+# Predictive models showed which cities were at higher risk, helping governments decide when to do lockdowns.
+#  Data on hospital capacity was important to ensure resources were allocated where they were needed the most.
+
+
+# Step 3:  Imagine if governments didnt know where the virus was spreading. How would they have made decisions? How did data analysis help in making the right decisions during the crisis?
+
+# One big example: when COVID first hit, data was collected about how the virus spread. By analyzing things like travel patterns and infection rates, experts were able to predict which cities were at higher risk, helping to plan medical needs and lockdown measures.
+
+# Step 4: Why This Matters:
+# Data analysis was more than just helpful; it was critical in guiding how the world responded to COVID-19. It helped us understand where to focus medical resources, how to communicate risks, and even aided vaccine development by analyzing clinical trial data. So, without data analysis, many of the critical decisions that saved lives would have been much harder to make.
